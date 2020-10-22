d85c9100e8fc72017fbd9738ba46ee373831fbd6
@anulman <anulman@gmail.com>
2020-10-22 00:14:43 -0400

    feat(pages/index): implement <Button> component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,10 @@
 import React, { ReactNode } from 'react';
 
+import { Button } from '../components/Button';
+
 export default function HomePage(): ReactNode {
-  return <div>Welcome to Next.js!</div>;
+  return <main>
+    <h1>Welcome to Next.js!</h1>
+    <Button type='button'>im a linaria-styled button</Button>
+  </main>;
 }
