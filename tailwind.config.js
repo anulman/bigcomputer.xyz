5899a357d4639383c717f1c6050273b339a5f845
@anulman <anulman@gmail.com>
2020-10-24 00:17:33 -0400

    feat(tailwind): init config

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  purge: [
+    './src/**/*.{ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
