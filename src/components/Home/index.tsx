d35c87ea02661de32653ba8cbec8660302ed8d29
@anulman <anulman@gmail.com>
2020-10-25 19:54:03 -0400

    feat(components/Home): implement `<AboutText />`

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useResponsive } from 'ahooks';
 
+import { AboutText } from './AboutText';
 import { Hero } from './Hero';
 import { Quotes } from './Quotes';
 import { Page } from '../../parts/Page';
@@ -14,6 +15,7 @@ export const Home = (): JSX.Element => {
       {!md && (
         <Quotes className="my-2 flex flex-col p-4 items-center" />
       )}
+      <AboutText className="mt-6 w-11/12 max-w-4xl mx-auto" />
     </Page>
   );
 };
