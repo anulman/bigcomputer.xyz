5fd619017b89017bce263503fb636d8b1fff35ee
@anulman <anulman@gmail.com>
2020-10-25 19:51:31 -0400

    feat(components/Home): init `<AboutText />`

---
+++ src/components/Home/AboutText.tsx
@@ -0,0 +1,15 @@
+import React, { HTMLProps } from 'react';
+
+export const AboutText = (props: HTMLProps<HTMLDivElement>): JSX.Element => (
+  <div {...props}>
+    <p>
+      <em>The Tale of the Big Computer</em> is an uncannily prescient piece of
+      science fiction history. Written in 1966 by a Novel Laureate-to-be, this
+      novel describes, from an unspecified future vantage point, the digital
+      evolution that culminates in The Computer Age.
+    </p>
+    <p>
+      TKTKTK
+    </p>
+  </div>
+);
