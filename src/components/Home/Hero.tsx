df6b00227ba74f22a40a8c38928e17fa64398ee4
@anulman <anulman@gmail.com>
2020-10-25 19:47:52 -0400

    feat(components/Home): implement `<Quotes />` component

--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -1,17 +1,28 @@
 import React from 'react';
 import { css } from 'linaria';
 import { styled } from 'linaria/react';
+import { useResponsive } from 'ahooks';
 
+import { Quotes } from './Quotes';
+import { Button } from '../../parts/Button';
 import { Image } from '../../parts/Image';
-import { H1 } from '../../parts/Text';
+import { H1, H2 } from '../../parts/Text';
 
 const HeroBackground = styled.div`
   @apply relative;
+
   height: 90vh;
+  text-shadow: 1px 1px 2px black;
+
+  @screen md {
+    min-height: 30rem;
+  }
 `;
 
 const HeroContents = styled.div`
-  @apply absolute inset-0 mt-4 flex justify-center text-white text-center;
+  @apply absolute inset-0 flex flex-col items-center text-white text-center;
+
+  top: 10%;
 `;
 
 // todo - rm these once `linaria` plays nice with pre-built components
@@ -20,17 +31,22 @@ const heroImageClassName = css`
   @apply w-full h-full object-cover object-bottom;
 `;
 
-const titleClassName = css`
-  text-shadow: 1px 1px 2px black;
-  font-family: Audiowide, cursive;
-`;
+export const Hero = (): JSX.Element => {
+  const { md } = useResponsive() ?? {};
 
-export const Hero = (): JSX.Element => (
-  <HeroBackground>
-    <Image className={heroImageClassName} imagePath={'hero.jpg'} />
+  return (
+    <HeroBackground>
+      <Image className={heroImageClassName} imagePath={'hero.jpg'} />
 
-    <HeroContents>
-      <H1 className={titleClassName}>The Tale of the Big Computer</H1>
-    </HeroContents>
-  </HeroBackground>
-);
+      <HeroContents>
+        <H1 className="bg-black p-2 w-full">The Tale of the Big Computer</H1>
+        <H2 className="mt-4 2xl:mt-8">A 1960s novel about technology&nbsp;today</H2>
+        <Button className="mt-4 2xl:mt-8 2xl:text-2xl">Preorder now!</Button>
+
+        {md && (
+          <Quotes className="flex w-10/12 max-w-6xl mt-auto mb-12 justify-center 2xl:text-2xl" />
+        )}
+      </HeroContents>
+    </HeroBackground>
+  );
+};
--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import { useResponsive } from 'ahooks';
 
 import { Hero } from './Hero';
+import { Quotes } from './Quotes';
 import { Page } from '../../parts/Page';
 
-export const Home = (): JSX.Element => (
-  <Page>
-    <Hero />
-  </Page>
-);
+export const Home = (): JSX.Element => {
+  const { md } = useResponsive() ?? {};
+
+  return (
+    <Page>
+      <Hero />
+      {!md && (
+        <Quotes className="my-2 flex flex-col p-4 items-center" />
+      )}
+    </Page>
+  );
+};
 
 export default Home;
