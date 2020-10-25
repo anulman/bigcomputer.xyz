1b4c93647f218d58bd576033f9e8dbb7b69e00fa
@anulman <anulman@gmail.com>
2020-10-25 19:46:27 -0400

    feat(tailwind): define screen sizes && add 2xl

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,13 @@ module.exports = {
     './src/**/*.{ts,tsx}',
   ],
   theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1681px',
+    },
     extend: {},
   },
   variants: {},
