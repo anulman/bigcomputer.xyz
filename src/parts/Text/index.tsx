feab8f50fc221cc442b6129484007748d1d9e61d
@anulman <anulman@gmail.com>
2020-10-25 19:45:55 -0400

    feat(parts/Text): add H2 && 2xl styles

--- src/parts/Text/index.tsx
+++ src/parts/Text/index.tsx
@@ -1,6 +1,19 @@
 import { styled } from 'linaria/react';
 
 export const H1 = styled.div`
-  font-size: 2.25rem;
+  @apply text-4xl leading-none;
+  font-family: Audiowide, cursive;
+
+  @screen 2xl {
+    @apply text-5xl;
+  }
+`;
+
+export const H2 = styled.div`
+  @apply text-3xl leading-tight;
   font-family: serif;
+
+  @screen 2xl {
+    @apply text-4xl;
+  }
 `;
