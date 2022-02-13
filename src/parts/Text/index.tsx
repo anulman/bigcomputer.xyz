c67a8e213e10c6459bee102cb2d35ea425eae2e2
@anulman <anulman@gmail.com>
2022-02-13 15:42:02 -0500

    Add <Link /> text component

--- src/parts/Text/index.tsx
+++ src/parts/Text/index.tsx
@@ -1,4 +1,9 @@
+import _ from 'lodash';
+import React from 'react';
 import { styled } from 'linaria/react';
+
+type LinkAttributes = { href: string; onClick: never } & Partial<React.HTMLAttributes<HTMLAnchorElement>>;
+type ButtonAttributes = { href: never; onClick: React.MouseEventHandler<HTMLButtonElement> } & Partial<React.HTMLAttributes<HTMLButtonElement>>;
 
 export const H1 = styled.div`
   @apply text-4xl leading-none;
@@ -17,3 +22,10 @@ export const H2 = styled.div`
     @apply text-4xl;
   }
 `;
+
+export const Link = styled<LinkAttributes | ButtonAttributes>(
+  ({ href, ...props }) =>
+    href !== undefined
+      ? <a href={href} {...props} />
+      : <button type={props.type ?? 'button'} {..._.omit(props, 'type')} />
+);
