14f99ce12fbc9417732692b0b965ede1708ee18f
@anulman <anulman@gmail.com>
2022-03-13 15:56:14 -0400

    rm deprecated code

--- src/parts/Button/Button.tsx
+++
@@ -1,5 +0,0 @@
-import { styled } from 'linaria/react';
-
-export const Button = styled.button`
-  @apply py-3 px-4 bg-pink-600;
-`;
--- src/parts/Page/index.tsx
+++
@@ -1,6 +0,0 @@
-import { styled } from 'linaria/react';
-
-export const Page = styled.main`
-  min-height: 100vh;
-  min-width: 100%;
-`;
