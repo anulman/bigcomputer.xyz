72807587c0c13041669e1c61061452f3cced4344
@anulman <anulman@gmail.com>
2022-03-24 11:32:47 -0700

    Install Fathom analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import { ReactNode, useEffect } from 'react';
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
 import { configResponsive } from 'ahooks';
+import * as Fathom from 'fathom-client';
 
 import tailwindConfig from '../../tailwind.config';
 
@@ -8,6 +10,20 @@ import '@src/css/tailwind.css';
 import '@src/css/app.css';
 
 export default function MyApp({ Component, pageProps }: AppProps): ReactNode {
+  const router = useRouter();
+
+  useEffect(() => {
+    Fathom.load('IYSQIMIJ', {
+      includedDomains: ['bigcomputer.xyz', 'www.bigcomputer.xyz'],
+    });
+
+    const onRouteChangeComplete = () => Fathom.trackPageview();
+
+    router.events.on('routeChangeComplete', onRouteChangeComplete);
+
+    return router.events.off('routeChangeComplete', onRouteChangeComplete);
+  }, []);
+
   useEffect(() => {
     console.info('This website is open source! See how it\'s made at https://github.com/anulman/bigcomputer.xyz');
     configResponsive(mapTailwindConfigToResponsiveConfig(tailwindConfig));
