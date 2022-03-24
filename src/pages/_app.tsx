e607f8b51557f0c56e7b75035bb6fb89c417e394
@anulman <anulman@gmail.com>
2022-03-24 11:54:45 -0700

    Init opengraph tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { ReactNode, useEffect } from 'react';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import { useRouter } from 'next/router';
 import { configResponsive } from 'ahooks';
 import * as Fathom from 'fathom-client';
@@ -29,7 +30,18 @@ export default function MyApp({ Component, pageProps }: AppProps): ReactNode {
     configResponsive(mapTailwindConfigToResponsiveConfig(tailwindConfig));
   }, []);
 
-  return <Component {...pageProps} />;
+  return <>
+    <Head>
+      <meta property="og:url" content={`https://www.bigcomputer.xyz${router.pathname}`} key="og:url" />
+      <meta property="og:type" content="website" key="og:type" />
+      <meta property="og:title" content="Tale of the Big Computer" key="og:title" />
+      <meta property="og:description" content="An uncannily-prescient 1960s science fiction novel; reprinted in English for the first time in 50 years" key="og:description" />
+      <meta property="og:image" content="https://bigcomputer.xyz/assets/images/3d-heart.png" key="og:image" />
+
+      <meta property="twitter:card" content="summary_large_image" key="twitter:card" />
+    </Head>
+    <Component {...pageProps} />
+  </>;
 }
 
 function mapTailwindConfigToResponsiveConfig(config: typeof tailwindConfig) {
