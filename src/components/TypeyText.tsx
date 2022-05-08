2ccb22bfe13c1021e09f8bd3fb107afa551b580c
@anulman <anulman@gmail.com>
2022-05-07 22:15:31 -0400

    Add [tap/enter] to continue prompts

--- src/components/TypeyText.tsx
+++ src/components/TypeyText.tsx
@@ -1,7 +1,11 @@
 import * as React from 'react';
 import { styled } from 'linaria/react';
+import * as rxjs from 'rxjs';
+import * as rx from 'rxjs/operators';
 import * as windups from 'windups';
 import VisuallyHidden from '@reach/visually-hidden';
+
+import { isTouchDevice } from '@src/utils/is-touch-device';
 
 type Props = React.PropsWithChildren<React.HTMLAttributes<HTMLDivElement> & {
   isPaused?: boolean;
@@ -14,6 +18,44 @@ const DEFAULT_BEAT_MS = 300;
 const DEFAULT_START_DELAY_BEATS = 4;
 // const BEAT_MS = 6;
 
+const onSpaceBar$ = typeof window === 'undefined'
+  ? rxjs.EMPTY
+  : rxjs.fromEventPattern<KeyboardEvent>(
+    (handler) => window.addEventListener('keydown', handler, { capture: true }),
+    (handler) => window.removeEventListener('keydown', handler, { capture: true }),
+  ).pipe(
+    rx.filter((event) => event.key === ' ' || event.code === 'Space' || event.keyCode === 32),
+    rx.tap((event) => event.preventDefault())
+  );
+
+const onTapScreen$ = typeof window === 'undefined'
+  ? rxjs.EMPTY
+  : rxjs.defer(() => rxjs.race(
+    rxjs.fromEventPattern<MouseEvent>(
+      (handler) => window.addEventListener('mousedown', handler, { capture: true }),
+      (handler) => window.removeEventListener('mousedown', handler, { capture: true }),
+    ),
+    rxjs.fromEventPattern<TouchEvent>(
+      (handler) => window.addEventListener('touchstart', handler, { capture: true }),
+      (handler) => window.removeEventListener('touchstart', handler, { capture: true }),
+    ),
+    rxjs.fromEventPattern<PointerEvent>(
+      (handler) => window.addEventListener('pointerdown', handler, { capture: true }),
+      (handler) => window.removeEventListener('pointerdown', handler, { capture: true }),
+    ),
+  ))
+    .pipe(
+      rx.filter(() => isTouchDevice),
+      rx.tap((event) => event.preventDefault())
+    );
+
+const ContinuePrompt = styled.p<{ isShowing?: boolean }>`
+  @apply italic;
+
+  opacity: ${({ isShowing }) => isShowing === true ? 0.7 : 0};
+  transition: opacity 0.5s ease-in;
+`;
+
 export const Content = styled<Props>(({
   children,
   isPaused = false,
@@ -25,6 +67,7 @@ export const Content = styled<Props>(({
   const divRef = React.useRef<HTMLDivElement>();
   const [hasStarted, setHasStarted] = React.useState(false);
   const [numCompleted, setNumCompleted] = React.useState(0);
+  const [isPromptingToContinue, setIsPromptingToContinue] = React.useState(false);
 
   // delay manually with our own state, because windups seems not to respect
   // <Pause>:first-child elems
@@ -66,19 +109,40 @@ export const Content = styled<Props>(({
       : [children],
     [numCompleted],
   );
-  const incrementNumCompleted = React.useCallback(() => setTimeout(() => setNumCompleted((_numCompleted) => _numCompleted + 1), 3 * beatMs), []);
+
+  const onWindupFinished = React.useCallback(() => {
+    setIsPromptingToContinue(true);
+    rxjs.race(onSpaceBar$, onTapScreen$)
+      .pipe(rx.take(1), rx.tap(() => setIsPromptingToContinue(false)), rx.delay(500))
+      .subscribe(() => setNumCompleted((_numCompleted) => _numCompleted + 1));
+  }, []);
 
   return <>
     <div aria-hidden ref={divRef} {...props}>
       <span />
       {renderedChildren.map((child, index) => (
-        <windups.WindupChildren isPaused={!hasStarted || isPaused} key={`typey-text-${index}`} onFinished={incrementNumCompleted}>
+        <windups.WindupChildren
+          isPaused={!hasStarted || isPaused}
+          key={`typey-text-${index}`}
+          onFinished={onWindupFinished}
+        >
           <windups.OnChar fn={triggerReflow}>
             {child}
           </windups.OnChar>
         </windups.WindupChildren>
       ))}
     </div>
+
+    <ContinuePrompt aria-hidden isShowing={
+      isPromptingToContinue &&
+      children instanceof Array &&
+      (numCompleted + 1) < children.length
+    }>
+      {isTouchDevice
+        ? <>[tap screen to continue]</>
+        : <>[press spacebar to continue]</>}
+    </ContinuePrompt>
+
     <VisuallyHidden>{children}</VisuallyHidden>
   </>;
 })`
