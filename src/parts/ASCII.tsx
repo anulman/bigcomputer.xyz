34f29be5935a238cb92ca0f753315e5287d39ddb
@anulman <anulman@gmail.com>
2022-07-10 13:45:10 -0400

    add collection of ASCII parts

---
+++ src/parts/ASCII.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import { styled } from 'linaria/react';
+
+export const Title = styled<React.HTMLAttributes<HTMLPreElement>>((props) => <pre {...props}>
+&nbsp;&nbsp;__  .__               __         .__                   _____    __  .__            <br />
+_/  |_|  |__   ____   _/  |______  |  |   ____     _____/ ____\ _/  |_|  |__   ____  <br />
+\   __\  |  \_/ __ \  \   __\__  \ |  | _/ __ \   /  _ \   __\  \   __\  |  \_/ __ \ <br />
+&nbsp;|  | |   Y  \  ___/   |  |  / __ \|  |_\  ___/  (  &lt;_&gt; )  |     |  | |   Y  \  ___/ <br />
+&nbsp;|__| |___|  /\___  &gt;  |__| (____  /____/\___  &gt;  \____/|__|     |__| |___|  /\___  &gt;<br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\/     \/             \/          \/                            \/     \/ <br />
+&nbsp;&nbsp;&nbsp;__ ___.   .__         __                                        __                <br />
+&nbsp;&nbsp;/ / \_ |__ |__| ____   \ \     ____  ____   _____ ______  __ ___/  |_  ___________ <br />
+&nbsp;/ /   | __ \|  |/ ___\   \ \  _/ ___\/  _ \ /     \\____ \|  |  \   __\/ __ \_  __ \<br />
+&nbsp;\ \   | \_\ \  / /_/  &gt;  / /  \  \__(  &lt;_&gt; )  Y Y  \  |_&gt; &gt;  |  /|  | \  ___/|  | \/<br />
+&nbsp;&nbsp;\_\  |___  /__\___  /  /_/    \___  &gt;____/|__|_|  /   __/|____/ |__|  \___  &gt;__|   <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\/  /_____/              \/            \/|__|                    \/       <br />
+</pre>)``;
