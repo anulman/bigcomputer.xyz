f73599bcbd32ef867a699c01ba2b9b78e505aec6
@anulman <anulman@gmail.com>
2022-08-06 15:54:46 -0400

    init base <StrikeThrough> text implementation

---
+++ src/parts/Text.tsx
@@ -0,0 +1,50 @@
+import _ from 'lodash';
+import * as React from 'react';
+import { styled } from 'linaria/react';
+import * as spring from '@react-spring/web';
+
+type Props = {
+  // todo - can we eventually support nodes && fragments?
+  children?: string;
+  isStruckThrough?: boolean;
+  asContainer?: Exclude<
+    keyof HTMLElementTagNameMap,
+    'dir' | 'font' | 'frame' | 'frameset' | 'marquee'
+  >
+};
+
+const Container = styled.span`@apply relative;`;
+
+export const StrikeThrough = styled(
+  (props: Parameters<typeof Container>[0] & Props) => {
+    const springs = spring.useTrail(props.children?.length ?? 0, {
+      config: { frequency: Math.min(0.1, 4/props.children?.length), damping: 88/100 },
+      from: { opacity: 1 },
+      opacity: props.isStruckThrough ? 1 : 0,
+    });
+
+    return (
+      <Container as={props.asContainer} {..._.omit(props, 'children', 'isStruckThrough', 'asContainer')}>
+        <span>{props.children}</span>
+        <span className="fg" aria-hidden={true}>
+          {springs.map((styles, index) => {
+            return <spring.animated.span style={styles} key={`char${index}`}>{props.children[index]}</spring.animated.span>;
+          })}
+        </span>
+      </Container>
+    );
+  }
+)`
+  > .fg {
+    @apply absolute left-0 top-0;
+    z-index: 1;
+
+    color: transparent;
+    pointer-events: none;
+    user-select: none;
+
+    text-decoration: line-through;
+    text-decoration-color: limegreen;
+    text-decoration-thickness: 0.25rem;
+  }
+`;
