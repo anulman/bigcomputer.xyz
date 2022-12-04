e3930a1e6916a487bcf4db3ec217449b2b082802
@anulman <anulman@gmail.com>
2022-12-04 10:49:32 -0600

    add <text.Price> component

--- src/parts/Text.tsx
+++ src/parts/Text.tsx
@@ -15,6 +15,9 @@ type Props = {
 
 const Container = styled.span`@apply relative;`;
 
+const priceFormatter = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'usd' });
+export const Price = ({ amount }: { amount: number }) => <>{priceFormatter.format(amount / 100)}</>;
+
 const InnerStrikeThrough = (props: Parameters<typeof Container>[0] & Props) => {
   const springs = spring.useTrail(props.children?.length ?? 0, {
     config: { frequency: Math.min(0.1, 4/props.children?.length), damping: 88/100 },
