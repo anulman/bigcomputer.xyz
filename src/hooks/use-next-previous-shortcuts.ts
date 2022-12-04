81c3f6ce307a251a27c2ae20747b5a168d74dde9
@anulman <anulman@gmail.com>
2022-12-04 10:48:31 -0600

    add up/down arrows to nextPrev shortcuts

--- src/hooks/use-next-previous-shortcuts.ts
+++ src/hooks/use-next-previous-shortcuts.ts
@@ -1,14 +1,18 @@
 import * as React from 'react';
 
 type GoToCallback = (direction: 'next' | 'previous') => void;
+type UseArrowsOption = boolean | 'horizontal' | 'vertical';
 
-const isNextKey = (key: 'j' | 'k' | 'n' | 'p' | 'ArrowLeft' | 'ArrowRight') =>
-  key === 'j' || key === 'n' || key === 'ArrowRight';
+const NEXT_KEYS = ['j', 'n', 'ArrowRight', 'ArrowDown'] as const;
+const PREVIOUS_KEYS = ['k', 'p', 'ArrowLeft', 'ArrowUp'] as const;
+type NextPreviousKey = typeof NEXT_KEYS[number] | typeof PREVIOUS_KEYS[number];
 
-const isEmacsKey = (key: 'j' | 'k' | 'n' | 'p' | 'ArrowLeft' | 'ArrowRight') =>
-  key === 'n' || key === 'p';
+const isEmacsKey = (key: NextPreviousKey) => key === 'n' || key === 'p';
+const isVerticalKey = (key: NextPreviousKey) => key === 'ArrowUp' || key === 'ArrowDown';
+const isNextKey = <Key extends NextPreviousKey>(key: Key): boolean =>
+  (NEXT_KEYS as readonly string[]).includes(key);
 
-export const useNextPreviousShortcuts = (goToCallback: GoToCallback, { canUseTab = true, canUseArrows = true, canUseChars = true, charsRequireCtrl = false } = {}) => {
+export const useNextPreviousShortcuts = (goToCallback: GoToCallback, { canUseTab = true, canUseChars = true, charsRequireCtrl = false, useArrows = true as UseArrowsOption } = {}) => {
   React.useEffect(() => {
     const listener = (event: KeyboardEvent) => {
       switch (event.key) {
@@ -36,12 +40,17 @@ export const useNextPreviousShortcuts = (goToCallback: GoToCallback, { canUseTab
         break;
       case 'ArrowLeft':
       case 'ArrowRight':
-        if (!canUseArrows) {
+      case 'ArrowUp':
+      case 'ArrowDown': {
+        const axis = isVerticalKey(event.key) ? 'vertical' : 'horizontal';
+
+        if (useArrows === false || useArrows !== axis) {
           return;
         }
 
         goToCallback(isNextKey(event.key) ? 'next' : 'previous');
         break;
+      }
       default:
         return;
       }
@@ -53,5 +62,5 @@ export const useNextPreviousShortcuts = (goToCallback: GoToCallback, { canUseTab
     document.addEventListener('keydown', listener);
 
     return () => document?.removeEventListener('keydown', listener);
-  }, [goToCallback, canUseTab, canUseArrows, canUseChars, charsRequireCtrl]);
+  }, [goToCallback, canUseTab, useArrows, canUseChars, charsRequireCtrl]);
 };
